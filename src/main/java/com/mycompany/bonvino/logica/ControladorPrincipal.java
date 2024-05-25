1913710a82f2ba9958db935cda2603d8f896b053
MontarceLuciana <84165701+Lucilius21@users.noreply.github.com>
2024-05-25 19:54:24 -0300

    Update ControladorPrincipal.java
    Click derecho en el  proyecto -> Properties -> Run -> Main clase -> Browse -> com.mycompany.bonvino.igu.Principal

--- src/main/java/com/mycompany/bonvino/logica/ControladorPrincipal.java
+++ src/main/java/com/mycompany/bonvino/logica/ControladorPrincipal.java
@@ -1,4 +1,4 @@
-package com.mycompany.bonvino.logica;
+/*package com.mycompany.bonvino.logica;
 
 import com.mycompany.bonvino.igu.Principal;
 
@@ -9,4 +9,33 @@ public class ControladorPrincipal {
     princ.setVisible(true);
     princ.setLocationRelativeTo(null);
     }
+
+    // Método para tomar las fechas desde la ventanaGenerarReporte
+    public void tomarFechas(String fechaDesde, String fechaHasta) {
+        // Aquí puedes realizar las operaciones necesarias con las fechas obtenidas
+        System.out.println("Fecha desde: " + fechaDesde);
+        System.out.println("Fecha hasta: " + fechaHasta);
+    }
+
+} POR SI SE ROMPE FEROZMENTE
+*/
+
+package com.mycompany.bonvino.logica;
+
+import com.mycompany.bonvino.igu.Principal;
+
+public class ControladorPrincipal {
+    private Principal principal;
+
+    public ControladorPrincipal(Principal principal) {
+        this.principal = principal;
+    }
+
+    // Método para tomar las fechas desde VentanaGenerarReporte
+    public void tomarFechas(String fechaDesde, String fechaHasta) {
+        // Aquí puedes realizar las operaciones necesarias con las fechas obtenidas
+        System.out.println("Fecha desde: " + fechaDesde);
+        System.out.println("Fecha hasta: " + fechaHasta);
+    }
 }
+
