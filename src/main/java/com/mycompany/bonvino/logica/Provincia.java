dc71e3d9110ca9b4b4fb3e074c2a5088ea20d5f8
trashpandaspm <107225083+trashpandaspm@users.noreply.github.com>
2024-05-27 04:57:19 -0300

    Update Provincia.java

--- src/main/java/com/mycompany/bonvino/logica/Provincia.java
+++ src/main/java/com/mycompany/bonvino/logica/Provincia.java
@@ -1,11 +1,39 @@
 
 package com.mycompany.bonvino.logica;
 
-public class Provincia extends Pais {
-    String nombre;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-    public Provincia(String nombre) {
-        super(nombre);
-        this.nombre = nombre;
+public class Provincia extends Pais {
+    String nombreProvincia;
+
+    public Provincia(@JsonProperty("nombreProvincia") String nombreProvincia, 
+            @JsonProperty("nombrePais") String nombrePais) {
+        super(nombrePais);
+        this.nombreProvincia = nombreProvincia;
     }
-}
+
+    public String getNombre() {
+       return nombreProvincia;
+    }
+
+    public String getPais(String nom) {
+        String nomPais;
+        nomPais = "";
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            Pais [ ] paisArray = objectMapper.readValue(new File("paises.json"), Pais[ ].class);
+            for (Pais pais : paisArray){
+                if (this.nombrePais.equals(pais.getNombre())) {
+                   nomPais = pais.getNombre();
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Vino.class.getName()).log(Level.SEVERE, null, ex);
+    }
+        return nomPais;
+    }}
