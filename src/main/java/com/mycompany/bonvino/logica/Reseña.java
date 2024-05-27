c6ba39dd94965b09cc64f737e9f2f2b63bb66dcf
trashpandaspm <107225083+trashpandaspm@users.noreply.github.com>
2024-05-27 04:55:40 -0300

    Update Reseña.java

--- src/main/java/com/mycompany/bonvino/logica/Reseña.java
+++ src/main/java/com/mycompany/bonvino/logica/Reseña.java
@@ -1,16 +1,43 @@
 
 package com.mycompany.bonvino.logica;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Reseña {
     String comentario;
     Boolean esPremium;
-    int fechaResena;
+    String fechaResena;
     int puntaje;
+    int idVino;
 
-    public Reseña(String comentario, Boolean esPremium, int fechaResena, int puntaje) {
+    public Reseña(@JsonProperty("idVino") int idVino, 
+            @JsonProperty("comentario") String comentario, 
+            @JsonProperty("esPremium") Boolean esPremium, 
+            @JsonProperty("fechaReseña") String fechaResena, 
+            @JsonProperty("puntaje") int puntaje) {
         this.comentario = comentario;
         this.esPremium = esPremium;
         this.fechaResena = fechaResena;
         this.puntaje = puntaje;
+        this.idVino = idVino;
+    }
+
+    public boolean sosDePeriodo(String fechaDesde, String fechaHasta) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate desde = LocalDate.parse(fechaDesde, formatter);
+        LocalDate hasta = LocalDate.parse(fechaHasta, formatter);
+        LocalDate resena = LocalDate.parse(fechaResena, formatter);
+        
+        return resena.isBefore(hasta) && resena.isAfter(desde);
+    }
+
+    public boolean esPremium() {
+             return esPremium;
+    }
+
+    int getIdVino() {
+        return this.idVino;
     }
 }
