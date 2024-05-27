72281a3826e51d4702c7264d75ec6edf125af739
MontarceLuciana <84165701+Lucilius21@users.noreply.github.com>
2024-05-27 20:18:53 -0300

    Update Varietal.java
    actualización varietal

--- src/main/java/com/mycompany/bonvino/logica/Varietal.java
+++ src/main/java/com/mycompany/bonvino/logica/Varietal.java
@@ -1,12 +1,39 @@
-
 package com.mycompany.bonvino.logica;
 
 public class Varietal {
+    int id;
     String descripcion;
     int porcentajeComposicion;
 
-    public Varietal(String descripcion, int porcentajeComposicion) {
+    public Varietal(int id, String descripcion, int porcentajeComposicion) {
+        this.id = id;
         this.descripcion = descripcion;
         this.porcentajeComposicion = porcentajeComposicion;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getPorcentajeComposicion() {
+        return porcentajeComposicion;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPorcentajeComposicion(int porcentajeComposicion) {
+        this.porcentajeComposicion = porcentajeComposicion;
+    }
+    
+    
 }
